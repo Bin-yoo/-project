992b7bbe2b99efc76acb497d09917bb0a2be33ed
Bin-yoo <mapconn@live.com>
2018-06-12 22:27:25 +0800

    添加网页加载动画

--- project-zero/js/index.js
+++ project-zero/js/index.js
@@ -1,5 +1,10 @@
 $(function(){
-	
+
+	document.onreadystatechange = function() {
+		document.readyState == "complete";
+		$(".loading").fadeOut();
+	}
+
 	/* 刷新网页,背景图片文字出现动画 */
 	setTimeout(function() {
 		$('.text1').animate({'top':'300px','opacity':'1'},2000);
@@ -7,7 +12,7 @@ $(function(){
 	setTimeout(function() {
 		$('.text2').animate({'right':'120px','opacity':'1'},2000);
 	},1000);
-	
+
 	$('.part1 .page1_content_link .content_link').hover(function() {
 		$(this).find('p').stop(true).animate({'bottom':'0px'},500);
 	},function(){
