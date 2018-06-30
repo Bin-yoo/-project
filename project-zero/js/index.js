bfb1de9fec51050f9d0e30d129239dad131fa156
Bin-yoo <mapconn@live.com>
2018-06-30 23:06:59 +0800

    添加Bootstrap框架文件,修改与其不兼容的CSS字段

--- project-zero/js/index.js
+++ project-zero/js/index.js
@@ -16,6 +16,6 @@ $(function(){
 	$('.part1 .page1_content_link .content_link').hover(function() {
 		$(this).find('p').stop(true).animate({'bottom':'0px'},500);
 	},function(){
-		$(this).find('p').animate({'bottom':'-80px'},500);
+		$(this).find('p').animate({'bottom':'-90px'},500);
 	});
 });
