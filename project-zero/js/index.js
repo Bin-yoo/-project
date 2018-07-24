30daccaf57c376feb8746e9102c049e7a8e38d8f
weixiaoshou <807218823@qq.com>
2018-07-24 13:59:44 +0800

    修改part3排版，并完成响应式布局

--- project-zero/js/index.js
+++ project-zero/js/index.js
@@ -5,17 +5,19 @@ $(function(){
 		$(".loading").fadeOut();
 	}
 
-	/* 刷新网页,背景图片文字出现动画 */
-	setTimeout(function() {
-		$('.text1').animate({'top':'300px','opacity':'1'},2000);
-	},100);
-	setTimeout(function() {
-		$('.text2').animate({'right':'120px','opacity':'1'},2000);
-	},1000);
-
-	$('.part1 .page1_content_link .content_link').hover(function() {
-		$(this).find('p').stop(true).animate({'bottom':'0px'},500);
+    $('.part1 .content_link').hover(function() {
+        $(this).find('p').stop(true).animate({'bottom':'0px'},500);
+    },function(){
+        $(this).find('p').animate({'bottom':'-90px'},500);
+    });
+	
+	$(".part3 .eight_point .kuang").hover(function(){
+		$(this).css("border","3px solid red");
+		$(this).find("div").css("background-color","red");
+		$(this).find("div").css("color","yellow");
 	},function(){
-		$(this).find('p').animate({'bottom':'-80px'},500);
-	});
+		$(this).css("border","3px solid yellow");
+		$(this).find("div").css("background-color","yellow");
+		$(this).find("div").css("color","red");
+	})
 });
