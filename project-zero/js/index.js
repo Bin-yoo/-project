9aebfecb64d15f15788db15b4c4e5ae90f82a533
Bin-yoo <mapconn@live.com>
2018-08-07 15:38:43 +0800

    新加1part新闻,添加Scroll.js文件

--- project-zero/js/index.js
+++ project-zero/js/index.js
@@ -5,23 +5,6 @@ $(function(){
 		$(".loading").fadeOut();
 	}
 
-    $('.part1 .content_link').hover(function() {
-        $(this).find('p').stop(true).animate({'bottom':'0px'},500);
-    },function(){
-        $(this).find('p').animate({'bottom':'-90px'},500);
-    });
-
-
-	$(".part3 .eight_point .kuang").hover(function(){
-		$(this).css("border","3px solid red");
-		$(this).find("div").css("background-color","red");
-		$(this).find("div").css("color","yellow");
-	},function(){
-		$(this).css("border","3px solid yellow");
-		$(this).find("div").css("background-color","yellow");
-		$(this).find("div").css("color","red");
-	})
-
     $(window).resize(function(event) {
         var winwidth = $(window).width();
         if(winwidth <= 260){
@@ -30,6 +13,12 @@ $(function(){
         if(winwidth > 260){
             $("#n12 .media-num").css('top', '-70px');
         }
+    });
+
+    var Scroll_li = $(".box .section .part6 .newslist ul li").height();
+    $(".newslist").myScroll({
+        speed: 40,    //数值越大，速度越慢
+        rowHeight: 131    //li的高度
     });
 
 });
