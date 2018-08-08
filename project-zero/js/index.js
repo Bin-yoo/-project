e176c1da7f833e29fb222ab98d81db6754cbaa08
weixiaoshou <807218823@qq.com>
2018-08-08 15:10:30 +0800

    初步完成part布局

--- project-zero/js/index.js
+++ project-zero/js/index.js
@@ -15,6 +15,12 @@ $(function(){
         }
     });
 
+	$(".box .section .part5 .page5 .case .case_tab").hover(function(){
+		$(this).find(".case_button").stop(true).animate({right:'30px'},300);
+	},function(){
+		$(this).find(".case_button").stop(true).animate({right:'-34px'},300);
+	})
+	
     var Scroll_li = $(".box .section .part6 .newslist ul li").height();
     $(".newslist").myScroll({
         speed: 40,    //数值越大，速度越慢
