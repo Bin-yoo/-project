ce391c2691219a707ec2a3dfb7d63d537a14ccda
Bin-yoo <mapconn@live.com>
2018-08-10 20:47:53 +0800

    更新footer内容

--- project-zero/js/index.js
+++ project-zero/js/index.js
@@ -20,11 +20,56 @@ $(function(){
 	},function(){
 		$(this).find(".case_button").stop(true).animate({right:'-34px'},300);
 	})
-	
+
+    /* 滚动脚本 */
     var Scroll_li = $(".box .section .part6 .newslist ul li").height();
     $(".newslist").myScroll({
         speed: 40,    //数值越大，速度越慢
         rowHeight: 131    //li的高度
+    });
+
+    /*focus*/
+    var index = 0;
+    var textlength = $('.focus li').length;
+    var textwidth = $('.focus li').width();
+    var timer = null;
+
+    $('.focus').append($('.focus li').first().clone(true));
+
+    function textroll(){
+        timer = setInterval(function(){
+            index++;
+            if(index>textlength){
+                index=1;
+                $('.focus').css('left','0');
+                $('.focus').animate({'left':-index*textwidth}, 1000);
+                $('.f_handler i').eq(index).addClass('active').siblings().removeClass();
+            }else{
+                if(index>textlength-1){
+                    $('.focus').animate({'left':-index*textwidth}, 1000);
+                    $('.f_handler i').eq(0).addClass('active').siblings().removeClass();
+                }else{
+                    $('.focus').animate({'left':-index*textwidth}, 1000);
+                    $('.f_handler i').eq(index).addClass('active').siblings().removeClass();
+                }
+            }
+        },5000);
+    };
+    textroll();
+
+    /*鼠标移入动画停止*/
+    $('.theory').hover(function(){
+        clearInterval(timer);
+    },function(){
+        textroll();
+    });
+
+    /*小按钮*/
+    $('.f_handler i').click(function() {
+        var i = $(this).index();
+        index = i;
+        $(this).addClass('active').siblings().removeClass();
+        $('.focus').animate({'left':-index*textwidth}, 1000);
     });
 
 });
