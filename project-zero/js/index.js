ab1dd46531f601c55756e5c27135371076bd7b8d
Bin-yoo <mapconn@live.com>
2018-08-11 17:58:03 +0800

    更新footer轮播为响应式布局,完善首页功能

--- project-zero/js/index.js
+++ project-zero/js/index.js
@@ -20,7 +20,7 @@ $(function(){
 	},function(){
 		$(this).find(".case_button").stop(true).animate({right:'-34px'},300);
 	})
-	
+
 	/*理论点评子页*/
 	$(".box .section .lilun .case .case_tab").hover(function(){
 		$(this).find(".case_button").stop(true).animate({right:'30px'},300);
@@ -35,48 +35,97 @@ $(function(){
         rowHeight: 131    //li的高度
     });
 
-    /*focus*/
-    var index = 0;
-    var textlength = $('.focus li').length;
-    var textwidth = $('.focus li').width();
-    var timer = null;
-
-    $('.focus').append($('.focus li').first().clone(true));
-
-    function textroll(){
-        timer = setInterval(function(){
-            index++;
-            if(index>textlength){
-                index=1;
-                $('.focus').css('left','0');
-                $('.focus').animate({'left':-index*textwidth}, 1000);
-                $('.f_handler i').eq(index).addClass('active').siblings().removeClass();
-            }else{
-                if(index>textlength-1){
-                    $('.focus').animate({'left':-index*textwidth}, 1000);
-                    $('.f_handler i').eq(0).addClass('active').siblings().removeClass();
-                }else{
-                    $('.focus').animate({'left':-index*textwidth}, 1000);
-                    $('.f_handler i').eq(index).addClass('active').siblings().removeClass();
-                }
-            }
-        },5000);
-    };
-    textroll();
-
-    /*鼠标移入动画停止*/
-    $('.theory').hover(function(){
-        clearInterval(timer);
-    },function(){
-        textroll();
+    /* owl-carousel */
+    $(".owl-carousel").owlCarousel({
+        items : 1,
+        loop : true,
+        mergeFit : true,
+        dots : true,
+        autoplay : true,
+        autoplayTimeout : 5000,
+        autoplayHoverPause : true,
+        smartSpeed : 750
     });
 
-    /*小按钮*/
-    $('.f_handler i').click(function() {
-        var i = $(this).index();
-        index = i;
-        $(this).addClass('active').siblings().removeClass();
-        $('.focus').animate({'left':-index*textwidth}, 1000);
+    /* ScrollTo */
+    $(".nav_top").click(function(){
+        $.scrollTo('.ebg',1000);
+    });
+    $(".nav_part1").click(function(){
+        $.scrollTo('.Preface',1000);
+    });
+    $(".nav_part2").click(function(){
+        $.scrollTo('.part2',1000);
+    });
+    $(".nav_part3").click(function(){
+        $.scrollTo('.part3',1000);
+    });
+    $(".nav_part4").click(function(){
+        $.scrollTo('.part4',1000);
+    });
+    $(".nav_part5").click(function(){
+        $.scrollTo('.threebox>div>div>div>span',1000);
+    });
+    $(".nav_part6").click(function(){
+        $.scrollTo('.case_tab>a',1000);
+    });
+    $(".nav_footer").click(function(){
+        $.scrollTo('.footer',1000);
     });
 
+    $(window).scroll(function(){
+        console.log($(window).scrollTop());
+        if($(window).scrollTop() < 400){
+            $(".scroll1").addClass('active');
+        }else{
+            $(".scroll1").removeClass('active');
+        }
+        if($(window).scrollTop() > 400 && $(window).scrollTop() < 1100){
+            $(".scroll2").addClass('active');
+        }else{
+            $(".scroll2").removeClass('active');
+        }
+        if($(window).scrollTop() > 1100 && $(window).scrollTop() < 2100){
+            $(".scroll3").addClass('active');
+        }else{
+            $(".scroll3").removeClass('active');
+        }
+        if($(window).scrollTop() > 2100 && $(window).scrollTop() < 3150){
+            $(".scroll4").addClass('active');
+        }else{
+            $(".scroll4").removeClass('active');
+        }
+        if($(window).scrollTop() > 3150 && $(window).scrollTop() < 4250){
+            $(".scroll5").addClass('active');
+        }else{
+            $(".scroll5").removeClass('active');
+        }
+        if($(window).scrollTop() > 4250 && $(window).scrollTop() < 4850){
+            $(".scroll6").addClass('active');
+        }else{
+            $(".scroll6").removeClass('active');
+        }
+        if($(window).scrollTop() > 4850 && $(window).scrollTop() < 5100){
+            $(".scroll7").addClass('active');
+        }else{
+            $(".scroll7").removeClass('active');
+        }
+        if($(window).scrollTop() > 5100 && $(window).scrollTop() < 5400){
+            $(".scroll8").addClass('active');
+        }else{
+            $(".scroll8").removeClass('active');
+        }
+    });
+
+    /* backtop */
+    $(window).scroll(function(){
+        if($(window).scrollTop() > 1000){
+            $(".backtop").stop(true).fadeIn(500);
+        }else{
+            $(".backtop").fadeOut(500);
+        }
+    });
+    $(".backtop").click(function(){
+        $('body,html').animate({'scrollTop':'0'},400);
+    });
 });
